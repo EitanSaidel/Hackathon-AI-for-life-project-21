6e781bd8e52153415774971c3bbc9c666050bd29
eitansaidel <eitansaidel453@gmail.com>
2026-05-05 11:27:33 +0300

    Second Commit

--- main.py
+++ main.py
@@ -111,6 +111,9 @@ print("\nData check complete.")
 import torch
 from monai.networks.nets import AutoencoderKL
 print("\nStarting VAE")
-device = torch.device()
+device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
+print(f"Using computing device: {device}")
+
+
 
 
