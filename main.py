09a3c36d0231620a435fa80da4028085ada55958
eitansaidel <eitansaidel453@gmail.com>
2026-05-05 17:47:23 +0300

    Second Commit

--- main.py
+++ main.py
@@ -109,7 +109,6 @@ for batch_data in train_loader:
 
 print("\nData check complete.")
 import torch
-from monai.networks.nets import AutoencoderKL
 print("\nStarting VAE")
 device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
 print(f"Using computing device: {device}")
